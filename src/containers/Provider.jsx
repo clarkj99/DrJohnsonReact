ff1b20d0c47ec26ba717fdaac7bcd0ed26bc9194
clarkj99 <clark@stontech.net>
2020-01-12 01:42:43 -0500

    encounters list on provider.jsx

--- src/containers/Provider.jsx
+++ src/containers/Provider.jsx
@@ -17,34 +17,66 @@ class Provider extends React.Component {
       });
   };
 
+  state = { selectedEncounter: {} };
+
+  handleToggle = encounter => {
+    if (this.state.selectedEncounter.id === encounter.id)
+      this.setState({ selectedEncounter: {} });
+    else this.setState({ selectedEncounter: encounter });
+  };
+
   render() {
     return (
       <section className="section">
         <div className="columns">
-          <div className="column ">
-            <h2 className="title">Add A New Patient</h2>
-            <Link to="/provider/newpatient" className="button is-link is-large">
-              Create New Patient
-            </Link>
-          </div>
-
-          <div className="is-divider-vertical"></div>
-
           <div className="column">
             <h2 className="title">Open Encounters</h2>
             {this.props.encounters &&
               this.props.encounters.map(encounter => {
                 return (
-                  <div className="box " key={encounter.id}>
-                    <p>
-                      <span>
-                        {encounter.patient.last_name},{" "}
-                        {encounter.patient.first_name}
-                      </span>
-                      {" | "}
-                      <span>{encounter.appointment_at}</span>
-                    </p>
-                    <p>{encounter.complaint}</p>
+                  <div className="box">
+                    <div className="columns" key={encounter.id}>
+                      <div className="column">
+                        <div className="buttons">
+                          <button
+                            className="button"
+                            onClick={e => this.handleToggle(encounter)}
+                          >
+                            <span className="icon">
+                              <i
+                                className={
+                                  this.state.selectedEncounter.id ===
+                                  encounter.id
+                                    ? "fas fa-minus"
+                                    : "fas fa-plus"
+                                }
+                              ></i>
+                            </span>
+                          </button>
+                          <button className="button">
+                            <span className="icon">
+                              <i className="fas fa-edit"></i>
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                      <div className="column">
+                        <span>
+                          {encounter.patient.last_name},{" "}
+                          {encounter.patient.first_name}
+                        </span>
+                      </div>
+                      <div className="column">
+                        <span>
+                          {new Date(encounter.appointment_at).toLocaleString()}
+                        </span>
+                      </div>
+                    </div>
+                    {this.state.selectedEncounter.id === encounter.id && (
+                      <div className="">
+                        <span>{encounter.complaint}</span>
+                      </div>
+                    )}
                   </div>
                 );
               })}
@@ -52,21 +84,30 @@ class Provider extends React.Component {
 
           <div className="is-divider-vertical"></div>
 
-          <div className="column">
+          <div className="column is-one-fifth">
             <h2 className="title">Search</h2>
             <div className="field">
-              <div className="control has-icons-left is-loading">
+              <div className="control has-icons-left">
                 <span className="icon is-small is-left">
                   <i className="fas fa-search"></i>
                 </span>
 
                 <input
                   className="input"
-                  type="text"
+                  type="search"
                   placeholder="patient's last name"
+                  results={5}
                 ></input>
               </div>
             </div>
+          </div>
+          <div className="is-divider-vertical"></div>
+
+          <div className="column is-one-fifth">
+            <h2 className="title">Add A New Patient</h2>
+            <Link to="/provider/newpatient" className="button is-link ">
+              Create New Patient
+            </Link>
           </div>
         </div>
       </section>
